3fbfc4514c93fef0e3a039738de77800513de321
Alexander F. Rødseth <rodseth@gmail.com>
2019-06-05 15:23:07 +0200

    Patch release

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 const (
 	boxContentWidth = 42
-	versionString   = "botsay 1.2.1"
+	versionString   = "botsay 1.2.2"
 )
 
 // GFX is ASCII graphics as a string, and where to place it on the canvas
