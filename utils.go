e6eeaf46bafb0e8b5e71192b50645785c16a2738
Alexander F. Rødseth <rodseth@gmail.com>
2024-10-22 14:10:04 +0200

    Remove two unused functions

--- utils.go
+++ utils.go
@@ -28,42 +28,6 @@ func Dimensions(asciiArt string) (int, int) {
 	return maxWidth, maxHeight
 }
 
-// get returns a character at (x,y) in a multiline string.
-// If anything go wrong, or if (x,y) is out of bounds, it return a space rune.
-func get(s []rune, x, y, w, h int) rune {
-	if x < 0 || y < 0 {
-		return ' '
-	}
-	if x >= w || y >= h {
-		return ' '
-	}
-	// +1 to account for the trailing newlines
-	pos := y*w + x
-	if pos >= len(s) {
-		return ' '
-	}
-	r := s[pos]
-	switch r {
-	case '\n', '\t', '\r', '\v':
-		return ' '
-	}
-	return r
-}
-
-// toMap can convert from a multiline-string to an indexed slice of runes (y*w+x style)
-func toMap(s string, w int) []rune {
-	rs := make([]rune, 0)
-	for _, line := range strings.Split(s, "\n") {
-		rs = append(rs, []rune(line)...)
-		linelen := RuneLen(line)
-		if linelen < w {
-			// Fill out the rest of the line with spaces
-			rs = append(rs, []rune(strings.Repeat(" ", w-linelen))...)
-		}
-	}
-	return rs
-}
-
 // SplitWords can split a string into words, keeping punctuation and trailing spaces
 func SplitWords(s string) []string {
 	var (
